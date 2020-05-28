f36a9e124f2a75307c59500dfc00f48fb7e5df75
ArnavTheDevelloper <navdeepg@hpe.com>
2020-05-28 17:54:31 +0530

    ywsfygyukwdgjkwdgk

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var log6,joint;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -36,6 +37,9 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    log6 = new Log(100,100,100,PI/2);
+    joint = new Chain(bird.body,log6.body);
+
 }
 
 function draw(){
@@ -61,4 +65,7 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    log6.display();
+    joint.display();
 }
